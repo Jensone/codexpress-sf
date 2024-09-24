7007c89e3eaac84e5297e166c1c4fe03acf53b26
Jensone <hello@jensone.com>
2024-09-24 12:51:20 +0200

    ✨ (SubscriptionController): Implement routes for payment success, cancellation, and subscription checkout functionality in the controller

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -5,15 +5,17 @@ namespace App\Service;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
+/**
+ * Custom Class abstraite pour une implémentation
+ * simplifié des classes et dépendences du projet CodeXpress
+ */
 abstract class AbstractService
 {
-
     public function __construct(
         protected ParameterBagInterface $parameter, 
         protected MailerInterface $mailer)
     {
         $this->parameter = $parameter;
         $this->mailer = $mailer;
-
     }
 }
